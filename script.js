c84308d9b637f735f79308c5af10ac566cff1f39
naire416 <144183657+naire416@users.noreply.github.com>
2024-02-19 21:27:24 -0500

    image & text upload 1

---
+++ script.js
@@ -0,0 +1,41 @@
+// //object 1
+document.getElementById('image-top-1').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.getElementById('image-bottom-1').classList.toggle('active');
+    document.getElementById('text-1').classList.toggle('active');
+  });
+
+// //object 2
+document.getElementById('image-top-2').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.getElementById('image-bottom-2').classList.toggle('active');
+    document.getElementById('text-2').classList.toggle('active');
+    });
+
+ //object 3
+ document.getElementById('image-top-3').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.getElementById('image-bottom-3').classList.toggle('active');
+    document.getElementById('text-3').classList.toggle('active');
+    });
+
+    //object 4
+document.getElementById('image-top-4').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.getElementById('image-bottom-4').classList.toggle('active');
+    document.getElementById('text-4').classList.toggle('active');
+    });
+
+    //object 5
+document.getElementById('image-top-5').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.getElementById('image-bottom-5').classList.toggle('active');
+    document.getElementById('text-5').classList.toggle('active');
+    });
+
+//object 6
+document.getElementById('image-top-6').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.getElementById('image-bottom-6').classList.toggle('active');
+    document.getElementById('text-6').classList.toggle('active');
+  });
